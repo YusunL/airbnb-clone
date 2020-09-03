456b2eaee4126c3685e419c8acc0e1e17fe7861b
YusunL <47683668+YusunL@users.noreply.github.com>
2020-09-04 02:56:17 +0900

    UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+# @admin~ = decorater: same as admin.site.register(models.User, CustomUserAdmin)
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    pass
