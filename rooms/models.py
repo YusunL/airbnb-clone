22754328d723a652ce19e9ca85804a5dd749f9b1
YusunL <47683668+YusunL@users.noreply.github.com>
2020-09-08 21:09:14 +0900

    Models done (proto)

--- rooms/models.py
+++ rooms/models.py
@@ -6,6 +6,65 @@ from users import models as user_models
 # Create your models here.
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Model """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ House Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    # string으로 하면 import도 안해도 알아서 다 모델 찾아줌
+
+    def __str__(self):
+        return self.caption
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -24,4 +83,13 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    # on_delete: 만약 호스트가 삭제되면, cascade = 폭포효과 위에서 발생한 건 아래까지 영향을 미침: 호스트 삭제 = 방 삭제
+    # protected = 룸 있으면 호스트 삭제 불가/ set_null = 그냥 호스트 없는 상태로 존재
     # ForeignKey = connecting model to another model (only one sided) many-to-one relationship: many rooms-one user
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
